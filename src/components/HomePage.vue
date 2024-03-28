9cc1594d7d1c01e6db0a4ba8fc284c06b6348d43
Amit Jaisinghani <jaisinghaniamit121@gmail.com>
2024-03-29 03:12:21 +0530

    First Vue Js Commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,42 @@
+<template>
+<h1>Class Binding</h1>
+<h1 :class="applyStyle">Home Component</h1>
+<button v-on:click="colorfull=!colorfull">Apply Style</button>
+</template>
+
+<script>
+export default {
+    name: 'HomePage',
+    data() {
+        return {
+            colorfull: false,
+        }
+    },
+    computed: {
+        applyStyle() {
+            return {
+                green: this.colorfull,
+                err: true,
+                other:true,
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: orange;
+}
+
+.green {
+    background-color: grey;
+}
+
+.err {
+    color: red;
+}
+.other{
+  font-size: 20px;
+}
+</style>
